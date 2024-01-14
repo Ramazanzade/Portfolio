5db9dc6e0f5eb3b81dfa6367e301430fd0803a21
Nizam <ramazanov570633@gmail.com>
2024-01-14 17:37:26 +0400

    Update site -6

--- vite.config.ts
+++ vite.config.ts
@@ -14,10 +14,8 @@ export default defineConfig({
       plugins: [
         // ...
         gzipPlugin({
-          // Wähle die Dateitypen aus, die du komprimieren möchtest
           filter: /\.(js|css|html|json|svg)$/,
-          // Gzip-Komprimierungseinstellungen (optional)
-          minSize: 1024, // Mindestgröße der komprimierten Datei in Bytes
+          minSize: 1024, 
         }),
       ],
     },
